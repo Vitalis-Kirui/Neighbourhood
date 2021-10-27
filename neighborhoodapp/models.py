4f3d0dda73694cd8a3bf0c8b865421a856eade78
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-27 12:21:45 +0300

    creating profile class model

--- neighborhoodapp/models.py
+++ neighborhoodapp/models.py
@@ -33,3 +33,29 @@ class Neighbourhood(models.Model):
     def update_neighbourhood(self):
         neighbourhood_name = self.neighbourhood_name
         self.neighbourhood_name = neighbourhood_name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id_no = models.IntegerField(default=0)
+    email = models.CharField(max_length=30, blank=True)
+    profile_pic = CloudinaryField('profile')
+    bio = models.TextField(max_length=500, blank=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(cls, id):
+        Profile.objects.get(user_id=id)
