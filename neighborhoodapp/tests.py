6515e387acc482cde1de1fe246ada7173bf26894
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-27 12:59:16 +0300

    pushing succesful test for profile class model

--- neighborhoodapp/tests.py
+++ neighborhoodapp/tests.py
@@ -1,3 +1,34 @@
+import datetime
+from .models import Business, Neighbourhood, Post, Profile 
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+class ProfileTest(TestCase):
+    def setUp(self):
+     
+        self.user = User(username="Test", password="testingpassword")
+        self.user.save()
+        self.neighbourhood =  Neighbourhood(neighbourhood_name = "Sambu75", neighbourhood_location= "F-Society N", admin = self.user,neighbourhood_description='Home is best all the time', neighbourhood_photo="neighbourhood75.jpg")
+        self.neighbourhood.save()
+        self.profile = Profile(id=75,id_no=34931235,email='testingemail@g.com', profile_pic='profile.jpg', bio='Just testing profile class',neighbourhood=self.neighbourhood,
+                                    user=self.user)
+      
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_profile(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)
+
+    def test_delete_profile(self):
+        self.profile.delete_profile()
+        testsaved = Profile.objects.all()
+        self.assertFalse(len(testsaved) < 0)    
+
+    def test_update_profile(self):
+        self.profile.save_profile()
+        self.profile.update_profile(self.profile.user_id)
+        self.profile.save_profile()
+        self.assertTrue(Profile,self.profile.user)
+
