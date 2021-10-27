16b78b991ce6f309334cea9bf1abbf4fbb4d7580
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-27 12:16:37 +0300

    creating neighbourhood class model

--- neighborhoodapp/models.py
+++ neighborhoodapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField(max_length=200)
+    neighbourhood_location = models.CharField(max_length=200)
+    neighbourhood_description = models.TextField(max_length=500, blank=True)
+    neighbourhood_photo = CloudinaryField('photo', default='photo')
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin')
+
+    def __str__(self):
+        return self.neighbourhood_name
+
+    def save_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighbourhood(cls, neighbourhood_id):
+        return cls.objects.filter(id=hood_id)
+
+    @property
+    def occupants_count(self):
+        return self.neighbourhood_users.count()
+
+    def update_neighbourhood(self):
+        neighbourhood_name = self.neighbourhood_name
+        self.neighbourhood_name = neighbourhood_name
