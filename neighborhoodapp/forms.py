bf0f9c7bcdf06bcbe6bf725acb0ecc978a133a18
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-28 13:20:44 +0300

    pushing signup form and registration base html

---
+++ neighborhoodapp/forms.py
@@ -0,0 +1,12 @@
+from neighborhoodapp.models import Business, Neighbourhood, Post, Profile
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
