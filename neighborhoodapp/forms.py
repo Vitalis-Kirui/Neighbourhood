1366bc5b692aa71315449e2c11153d84117f9476
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-28 13:42:48 +0300

    creating update user and update profile forms

--- neighborhoodapp/forms.py
+++ neighborhoodapp/forms.py
@@ -10,3 +10,15 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+class UpdateUserForm(forms.ModelForm):
+    
+    class Meta:
+        model = User
+        fields = ('username',)
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['email','profile_pic', 'bio','neighbourhood']
